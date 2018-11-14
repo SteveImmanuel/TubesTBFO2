3bf5c802b4a7beb9c11a14acb5e7d812405b6880
a-johanes <antoniusjohanes99@gmail.com>
2018-11-14 23:02:07 +0700

    change a little bit definition in operation.h + relocate readme.md

--- src/operation.h
+++ src/operation.h
@@ -7,19 +7,33 @@
 #include "boolean.h"
 
 /*
-S -> T | S+T | S-T calculate
-T -> F | T*F | T/F timesdiv
-F -> (S) | -A | A | A.A | -A.A sign
-A -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | AA number
+S -> K | S+K | S-K      tambah kurang
+K -> P | P*K | P/K      kali bagi
+P -> I | I^P      pangkat
+I -> (S) | A | -A | A.A | -A.A      simbol
+A -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | AA angka
 */
-extern int panjangKata;
 
-boolean isNumber(char st);
-boolean isSymbol(char st);
-void Calculate(char* st, int*idx, float * result, boolean *valid);
-void TimesDiv(char* st,int* idx, float* result,boolean *valid);
-void Sign(char* st,int* idx, float* result,boolean *valid);
-void Number(char* st,int* idx, float* result);
-void FloatNumber(char* st,int* idx, float* result);
+extern int lenInput;
+
+boolean isNumber(char x);
+/*
+  fungsi bernilai True jika karakter yang dimasukkan adalah bilangan
+  dengan 0 <= x <= 9
+*/
+void BilanganKoma(char *persamaan, int *indeks, float *hasil);
+/*
+  I.S. persamaan dan indeks terdefinisi
+  F.S. prosedur mengembalikan bilangan 0. yang diawali dari indeks
+*/
+void TambahKurang(char* st, int*idx, float * result, boolean *valid);
+/*
+
+*/
+void KaliBagi(char* st,int* idx, float* result,boolean *valid);
+void Pangkat(char* st,int* idx, float* result,boolean *valid);
+void Simbol(char* st,int* idx, float* result,boolean *valid);
+void Angka(char* st,int* idx, float* result);
+
 
 #endif
